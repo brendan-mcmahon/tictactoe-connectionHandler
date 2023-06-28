0059b9fcd2903a57144bec47f6dcf150975c92b8
brendanjeffreymcmahon <brendan.mcmahon@bloomerang.com>
2023-06-28 17:27:30 -0400

    idk

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-const AWS = require('aws-sdk');
-  const ddb = new AWS.DynamoDB.DocumentClient();
-  
-  exports.handler = async (event, context) => {
-    const connectionId = event.requestContext.connectionId;
-    const playerName = event.queryStringParameters.playerName;
-  
-    const putParams = {
-      TableName: "tictactoe-games",
-      Item: {
-        connectionId: connectionId,
-        playerName: playerName,
-      }
-    };
-  
-    try {
-      await ddb.put(putParams).promise();
-      return { statusCode: 200, body: 'Connected.' };
-    } catch (err) {
-      console.error(err);
-      return { statusCode: 500, body: 'Failed to connect: ' + JSON.stringify(err) };
-    }
+const AWS = require("aws-sdk");
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event, context) => {
+  const connectionId = event.requestContext.connectionId;
+  const playerName = event.queryStringParameters.playerName;
+
+  const putParams = {
+    TableName: "tictactoe-games",
+    Item: {
+      connectionId: connectionId,
+      playerName: playerName,
+    },
   };
-  
-  
+
+  try {
+    await ddb.put(putParams).promise();
+    return { statusCode: 200, body: "Connected." };
+  } catch (err) {
+    console.error(err);
+    return { statusCode: 500, body: "Failed to connect: " + JSON.stringify(err) };
+  }
+};
